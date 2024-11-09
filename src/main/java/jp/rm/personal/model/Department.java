31e612f52d9b15d3daaf40a03190fd2457ff4674
dream <dream@192.168.11.7>
2024-11-10 05:21:21 +0900

    add:プロトタイプ作成のためにmodelパッケージのクラスを作成

---
+++ src/main/java/jp/rm/personal/model/Department.java
@@ -0,0 +1,29 @@
+package jp.rm.personal.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 40)
+	private String name;
+	
+	@OneToMany(mappedBy = "department")
+	private List<User> employee;
+}
---
+++ src/main/java/jp/rm/personal/model/User.java
@@ -0,0 +1,28 @@
+package jp.rm.personal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 40)
+	private String name;
+	
+	@ManyToOne
+	private Department department;
+	
+}
