6e18da955f35a4142f2f4b09724b5adcd39a1b1e
dream <dream@192.168.11.7>
2024-11-10 05:20:47 +0900

    add:プロトタイプ作成のためにcontrollerパッケージのクラスを作成

--- src/main/java/jp/rm/personal/controller/TaskController.java
+++ src/main/java/jp/rm/personal/controller/TaskController.java
@@ -1,5 +1,61 @@
 package jp.rm.personal.controller;
 
-public class TaskController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jp.rm.personal.model.User;
+import jp.rm.personal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class TaskController {
+	
+	private final UserRepository userRepository;
+	
+	@GetMapping("/")
+	public String showList(Model model) {
+		model.addAttribute("user", userRepository.findAll());
+		return "index";
+	}
+	
+	@GetMapping("/add")
+	public String addUser(User user) {
+		
+		return "form";
+	}
+	
+	@PostMapping("/process")
+	public String process(@Validated User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			
+			return "form";
+		}
+		
+		userRepository.save(user);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editUser(@PathVariable Long id, Model model) {
+		
+		model.addAttribute("user", userRepository.findById(id));
+		return "form";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteUser(@PathVariable Long id) {
+		
+		userRepository.deleteById(id);
+		return "redirect:/";
+	}
 }
+
+
