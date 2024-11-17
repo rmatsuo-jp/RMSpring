e05ca2d46b9d8e053371f5e577fee9bc33db62b3
rmatsuo-jp <dreamskyryou@gmail.com>
2024-11-17 18:39:26 +0900

    add:簡易的なログイン機能の実装
    configにてユーザ名、パスワードを平文で入力したものを認証する機能を追加。
    実用にはパスワードの暗号化およびDBでの管理が必要となる。

---
+++ src/main/java/jp/rm/personal/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package jp.rm.personal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();	//パスワードの暗号化に用いる
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+			.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+			
+			.formLogin(Customizer.withDefaults());
+		
+		return http.build();
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user")
+			.password(passwordEncoder().encode("user"))
+			.authorities("USER")
+			.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+}
